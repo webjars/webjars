aed0ca25575d084b5009145496a8b94180a30c41
James Ward <james@jamesward.org>
2012-10-09 17:58:50 -0600

    basic UI is working

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,19 +1,16 @@
 package controllers
 
 import play.api.mvc.{Action, Controller}
+import models.WebJars
 
 object Application extends Controller {
     
   def index = Action {
-    Ok(views.html.index())
+    Ok(views.html.index(WebJars.all))
   }
 
   def documentation = Action {
     Ok(views.html.documentation())
-  }
-
-  def examples = Action {
-    Ok(views.html.examples())
   }
   
 }
