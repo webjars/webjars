5616144c709340970659bd9ab2790716d91d3821
James Ward <james@jamesward.org>
2014-03-19 20:19:55 -0600

    pull WebJars from jsDelivr CDN

---
+++ app/controllers/StaticWebJarAssets.scala
@@ -0,0 +1,19 @@
+package controllers
+
+import play.api.mvc.{RequestHeader, Controller}
+import play.api.Play
+import play.api.Play.current
+
+object StaticWebJarAssets extends Controller {
+
+  def at(file: String) = Assets.at("/META-INF/resources/webjars", file)
+
+  def getUrl(file: String) = {
+    val maybeContentUrl = Play.configuration.getString("contentUrl")
+
+    maybeContentUrl.map { contentUrl =>
+        contentUrl + controllers.routes.StaticWebJarAssets.at(file).url
+    } getOrElse controllers.routes.StaticWebJarAssets.at(file).url
+  }
+
+}
