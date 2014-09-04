1c14a8538877c359dc7c9026eb4de57bd0428b34
James Ward <james@jamesward.org>
2014-09-03 22:14:16 -0600

    memory logging

--- app/utils/Global.scala
+++ app/utils/Global.scala
@@ -1,10 +1,13 @@
 package utils
 
-import play.api.{Application, Play}
+import play.api.{Logger, Application, Play}
 import play.api.libs.concurrent.Akka
 import play.api.mvc.WithFilters
 import play.filters.gzip.GzipFilter
 import shade.memcached.{AuthConfiguration, Configuration, Memcached}
+import scala.concurrent.ExecutionContext.Implicits.global
+
+import scala.concurrent.duration._
 
 object Global extends WithFilters(new GzipFilter()) {
 
@@ -27,6 +30,15 @@ object Global extends WithFilters(new GzipFilter()) {
     val memcachedDispatcher = Akka.system(Play.current).dispatchers.lookup("memcached.dispatcher")
 
     Memcached(Configuration(Play.current.configuration.getString("memcached.servers").get, authConfig), memcachedDispatcher)
+  }
+
+
+  override def onStart(app: Application): Unit = {
+    super.onStart(app)
+
+    Akka.system(app).scheduler.schedule(Duration.Zero, 1.minute, new Runnable {
+      override def run(): Unit = Logger.info("Free Mem: " + Runtime.getRuntime.freeMemory() + " Max Mem: " + Runtime.getRuntime.maxMemory() + " Total Mem: " + Runtime.getRuntime.totalMemory())
+    })
   }
 
   override def onStop(app: Application): Unit = {
