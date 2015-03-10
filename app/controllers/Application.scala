e8db53c1f08566b91e5b83ad809614e64c7867f7
James Ward <james@jamesward.org>
2015-03-10 08:04:51 -0600

    too many undos

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -154,7 +154,7 @@ object Application extends Controller {
     request.flash.get(X_GITHUB_ACCESS_TOKEN).map { accessToken =>
       github.user(accessToken).map { user =>
         val login = (user \ "login").as[String]
-        Ok(views.html.webJarRequest(weSome(accessToken), Some(login)))
+        Ok(views.html.webJarRequest(webJarRequestForm, Some(accessToken), Some(login)))
       }
     } getOrElse {
       Future.successful(Ok(views.html.webJarRequest(webJarRequestForm)))
