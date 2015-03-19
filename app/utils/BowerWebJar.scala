6da13d712fccd6a20b31823d4b775e671d19697c
James Ward <james@jamesward.org>
2015-03-19 11:58:20 -0600

    do not exit until final message is delivered

--- app/utils/BowerWebJar.scala
+++ app/utils/BowerWebJar.scala
@@ -36,75 +36,69 @@ object BowerWebJar extends App {
       Future.sequence(maybeMavenDeps).map(_.toMap)
     }
 
-    // this needs it's own ws since the deployment ws will be closed on an error so we can't send the error out
-    def push(event: String, message: String): Unit = {
-      StandaloneWS.withWs { implicit ws =>
-        val pusher = Pusher(executionContext, ws.client, config)
-        maybepusherChannelId.fold(Future.successful[JsValue](JsNull)) { pusherChannelId =>
-          pusher.push(pusherChannelId, event, message)
-        }
-      }
-    }
-
     StandaloneWS.withWs { implicit ws =>
 
       val bower = Bower(executionContext, ws.client)
       val binTray = BinTray(executionContext, ws, config)
+      val pusher = Pusher(executionContext, ws.client, config)
+
+      def push(event: String, message: String): Future[JsValue] = {
+        maybepusherChannelId.fold(Future.successful[JsValue](JsNull)) { pusherChannelId =>
+          pusher.push(pusherChannelId, event, message)
+        }
+      }
       
       val webJarFuture = for {
         packageInfo <- bower.info(name, version)
-        _ = push("update", "Got Bower info")
+        _ <- push("update", "Got Bower info")
         mavenDependencies <- convertNpmDependenciesToMaven(packageInfo.dependencies)
-        _ = push("update", "Converted dependencies to Maven")
+        _ <- push("update", "Converted dependencies to Maven")
         pom = views.xml.pom(packageInfo, mavenDependencies).toString()
-        _ = push("update", "Generated POM")
+        _ <- push("update", "Generated POM")
         zip <- bower.zip(packageInfo.artifactId, version)
-        _ = push("update", "Fetched Bower zip")
+        _ <- push("update", "Fetched Bower zip")
         jar = WebJarUtils.createWebJar(zip, pom, packageInfo.artifactId, version)
-        _ = push("update", "Created WebJar")
+        _ <- push("update", "Created WebJar")
       } yield (packageInfo, pom, jar)
-      
-      val binTrayFuture = webJarFuture.flatMap { case (packageInfo, pom, jar) =>
+
+      webJarFuture.flatMap { case (packageInfo, pom, jar) =>
         // do not used the provided name because it may not be the canonical name
         val name = packageInfo.artifactId
         val packageName = s"$groupId:$name"
 
         for {
           createPackage <- binTray.getOrCreatePackage(binTraySubject, binTrayRepo, packageName, s"WebJar for $name", Seq("webjar", name), packageInfo.licenses, packageInfo.source, Some(packageInfo.homepage), packageInfo.issuesUrl.toOption, packageInfo.gitHubOrgRepo.toOption)
-          _ = push("update", "Created BinTray Package")
+          _ <- push("update", "Created BinTray Package")
           createVersion <- binTray.createVersion(binTraySubject, binTrayRepo, packageName, version, s"$name WebJar release $version", Some(s"v$version"))
-          _ = push("update", "Created BinTray Version")
+          _ <- push("update", "Created BinTray Version")
           publishPom <- binTray.uploadMavenArtifact(binTraySubject, binTrayRepo, packageName, s"$mavenBaseDir/$name/$version/$name-$version.pom", pom.getBytes)
           publishJar <- binTray.uploadMavenArtifact(binTraySubject, binTrayRepo, packageName, s"$mavenBaseDir/$name/$version/$name-$version.jar", jar)
           emptyJar = WebJarUtils.emptyJar()
           publishSourceJar <- binTray.uploadMavenArtifact(binTraySubject, binTrayRepo, packageName, s"$mavenBaseDir/$name/$version/$name-$version-sources.jar", emptyJar)
-          _ = push("update", "Published BinTray Assets")
+          _ <- push("update", "Published BinTray Assets")
           signVersion <- binTray.signVersion(binTraySubject, binTrayRepo, packageName, version)
-          _ = push("update", "Signed BinTray Assets")
+          _ <- push("update", "Signed BinTray Assets")
           publishVersion <- binTray.publishVersion(binTraySubject, binTrayRepo, packageName, version)
-          _ = push("update", "Published BinTray Version")
+          _ <- push("update", "Published BinTray Version")
           syncToMavenCentral <- binTray.syncToMavenCentral(binTraySubject, binTrayRepo, packageName, version)
-          _ = push("update", "Synced With Maven Central")
-        } yield syncToMavenCentral
+          _ <- push("update", "Synced With Maven Central")
+          _ <- push("success",
+            s"""Deployed!
+              |It will take a few hours for the Maven Central index to update but you should be able to start using the Bower WebJar now.
+              |GroupID = $groupId
+              |ArtifactID = ${packageInfo.artifactId}
+              |Version = ${packageInfo.version}
+            """.stripMargin)
+        } yield packageInfo
+      } recoverWith {
+        // push the error out and then return the original error
+        case e: Exception =>
+          push("failure",
+            s"""Failed!
+              |${e.getMessage}
+              |Please file an issue: https://github.com/webjars/webjars/issues
+            """.stripMargin).flatMap(_ => Future.failed(e))
       }
-
-      // return the packageInfo
-      val job = binTrayFuture.flatMap(_ => webJarFuture.map(_._1))
-
-      job.onComplete {
-        case Success(s) =>
-          push("success", "Deployed!")
-          push("success", "It will take a few hours for the Maven Central index to update but you should be able to start using the Bower WebJar now.")
-          push("success", "GroupID = " + groupId)
-          push("success", "ArtifactID = " + s.artifactId)
-          push("success", "Version = " + s.version)
-        case Failure(f) =>
-          push("failure", "Failed! " + f.getMessage)
-          push("failure", "Please file an issue: https://github.com/webjars/webjars/issues")
-      }
-
-
-      job
     }
   }
 
