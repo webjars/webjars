083177ac9a99453357c28ab1f4c509cc4eca0dcb
James Ward <james@jamesward.org>
2015-05-18 11:02:22 -0600

    fix cdn route for bower webjars

--- app/controllers/StaticWebJarAssets.scala
+++ app/controllers/StaticWebJarAssets.scala
@@ -9,11 +9,17 @@ import play.api.mvc.Controller
 object StaticWebJarAssets extends Controller {
 
   def at(file: String) = Assets.at("/META-INF/resources/webjars", file)
+  def atBower(file: String) = Assets.at("/META-INF/resources/webjars", file)
 
   lazy val maybeContentUrl = Play.configuration.getString("contentUrl")
 
   def getUrl(file: String) = {
     val atUrl = AssetRoutes.at(locate(file)).url
+    maybeContentUrl.fold(atUrl)(_ + atUrl)
+  }
+
+  def getUrlBower(file: String) = {
+    val atUrl = AssetRoutes.atBower(locate(file)).url
     maybeContentUrl.fold(atUrl)(_ + atUrl)
   }
 
