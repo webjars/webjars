dc23a301e6c2f137daa09645dbe4f06b979fda6c
James Ward <james@jamesward.org>
2016-07-12 20:34:59 -0600

    no more memcache compression

--- app/utils/Memcache.scala
+++ app/utils/Memcache.scala
@@ -1,18 +1,13 @@
 package utils
 
-import java.io.{ByteArrayInputStream, ByteArrayOutputStream}
-import java.util.zip.{DeflaterOutputStream, InflaterInputStream}
 import javax.inject.{Inject, Singleton}
 
 import play.api.Configuration
 import play.api.inject.ApplicationLifecycle
-import play.api.libs.json.Json
-import shade.memcached.{AuthConfiguration, Codec, Memcached}
+import shade.memcached.{AuthConfiguration, Memcached}
 
 import scala.concurrent.{ExecutionContext, Future}
-import scala.io.Source
 import scala.util.Try
-import scala.xml.{Elem, XML}
 
 @Singleton
 class Memcache @Inject() (configuration: Configuration, lifecycle: ApplicationLifecycle) (implicit ec: ExecutionContext) {
@@ -27,45 +22,5 @@ class Memcache @Inject() (configuration: Configuration, lifecycle: ApplicationLi
   }
 
   lifecycle.addStopHook(() => Future.fromTry(Try(instance.close())))
-
-}
-
-
-object Memcache {
-
-  // from: http://stackoverflow.com/questions/15079332/round-tripping-through-deflater-in-scala-fails
-
-  def compress(bytes: Array[Byte]): Array[Byte] = {
-    val deflater = new java.util.zip.Deflater
-    val baos = new ByteArrayOutputStream
-    val dos = new DeflaterOutputStream(baos, deflater)
-    dos.write(bytes)
-    dos.finish()
-    dos.close()
-    baos.close()
-    deflater.end()
-    baos.toByteArray
-  }
-
-  def decompress(bytes: Array[Byte]): Array[Byte] = {
-    val inflater = new java.util.zip.Inflater()
-    val bytesIn = new ByteArrayInputStream(bytes)
-    val in = new InflaterInputStream(bytesIn, inflater)
-    val out = Source.fromInputStream(in).map(_.toByte).toArray
-    in.close()
-    bytesIn.close()
-    inflater.end()
-    out
-  }
-
-  implicit object StringsCodec extends Codec[List[String]] {
-    def serialize(fileList: List[String]): Array[Byte] = compress(Json.toJson(fileList).toString().getBytes)
-    def deserialize(data: Array[Byte]): List[String] = Json.parse(decompress(data)).as[List[String]]
-  }
-
-  implicit object ElemCode extends Codec[Elem] {
-    def serialize(elem: Elem): Array[Byte] = compress(elem.toString().getBytes)
-    def deserialize(data: Array[Byte]): Elem = XML.loadString(new String(decompress(data)))
-  }
 
 }
--- app/utils/WebJarsFileService.scala
+++ app/utils/WebJarsFileService.scala
@@ -5,15 +5,13 @@ import javax.inject.Inject
 
 import play.api.http.Status
 import play.api.libs.ws.WSClient
-import shade.memcached.MemcachedCodecs._
 
-import scala.concurrent.duration.Duration
 import scala.concurrent.{ExecutionContext, Future}
 import scala.util.Try
 
-class WebJarsFileService @Inject() (memcache: Memcache, ws: WSClient) (implicit ec: ExecutionContext) {
+class WebJarsFileService @Inject() (ws: WSClient) (implicit ec: ExecutionContext) {
 
-  private def fetchFileList(groupId: String, artifactId: String, version: String): Future[List[String]] = {
+  def getFileList(groupId: String, artifactId: String, version: String): Future[List[String]] = {
     val url = s"http://webjars-file-service.herokuapp.com/listfiles/$groupId/$artifactId/$version"
     ws.url(url).get().flatMap { response =>
       response.status match {
@@ -24,24 +22,6 @@ class WebJarsFileService @Inject() (memcache: Memcache, ws: WSClient) (implicit 
         case _ =>
           Future.failed(new Exception(s"Error fetching $url : ${response.body}"))
       }
-    }
-  }
-
-  def getFileList(groupId: String, artifactId: String, version: String): Future[List[String]] = {
-    val cacheKey =  groupId + "-" + artifactId + "-" + version + "-files"
-
-    def fetchAndCache = {
-      val fileListFuture = fetchFileList(groupId, artifactId, version)
-      fileListFuture.foreach { fileList =>
-        memcache.instance.set(cacheKey, fileList, Duration.Inf)
-      }
-      fileListFuture
-    }
-
-    memcache.instance.get[List[String]](cacheKey).flatMap { maybeFileList =>
-      maybeFileList.fold(fetchAndCache)(Future.successful)
-    } recoverWith {
-      case e: Exception => fetchAndCache
     }
   }
 
