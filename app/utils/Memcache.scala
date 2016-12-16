6d15a0a1d9eb952bcfebdd7efa5eaffbf939de1d
James Ward <james@jamesward.org>
2016-12-16 16:22:29 -0700

    increase memcache timeout

--- app/utils/Memcache.scala
+++ app/utils/Memcache.scala
@@ -7,18 +7,19 @@ import play.api.inject.ApplicationLifecycle
 import shade.memcached.{AuthConfiguration, Memcached}
 
 import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.duration._
 import scala.util.Try
 
 @Singleton
 class Memcache @Inject() (configuration: Configuration, lifecycle: ApplicationLifecycle) (implicit ec: ExecutionContext) {
 
-  lazy val instance = {
+  lazy val instance: Memcached = {
     val maybeAuthConfig = for {
       username <- configuration.getString("memcached.username")
       password <- configuration.getString("memcached.password")
     } yield AuthConfiguration(username, password)
 
-    Memcached(shade.memcached.Configuration(configuration.getString("memcached.servers").get, maybeAuthConfig))
+    Memcached(shade.memcached.Configuration(addresses = configuration.getString("memcached.servers").get, authentication = maybeAuthConfig, operationTimeout = 30.seconds))
   }
 
   lifecycle.addStopHook(() => Future.fromTry(Try(instance.close())))
