fac0e8a7e0a7a55f44f232c07c1019b478c10ee9
James Ward <james@jamesward.com>
2018-02-26 04:41:46 -0700

    fix race with bintray test

--- test/utils/BinTraySpec.scala
+++ test/utils/BinTraySpec.scala
@@ -6,9 +6,11 @@ import java.util.Date
 import akka.util.Timeout
 import org.apache.commons.io.IOUtils
 import play.api.Environment
+import play.api.libs.json.JsArray
 import play.api.test._
 
 import scala.concurrent.duration._
+import scala.util.Random
 
 class BinTraySpec extends PlaySpecification with GlobalApplication {
 
@@ -21,31 +23,31 @@ class BinTraySpec extends PlaySpecification with GlobalApplication {
     if (application.configuration.getOptional[String]("bintray.username").isEmpty)
       "BinTray Auth" in skipped("skipped due to missing config")
     else {
-      // todo: should work when run multiple times in parallel
+      val repo = Random.alphanumeric.take(8).mkString
 
       "get packages" in {
-        await(binTray.getPackages("webjars", "test")).value must beEmpty
+        await(binTray.getPackages("webjars", "test")) must beAnInstanceOf[JsArray]
       }
       "create a package with an invalid gitHubRepo should fail" in {
-        await(binTray.createPackage("webjars", "test", "foo", "foo description", Seq("test"), Set("MIT"), new URI("http://github.com/webjars/webjars.git"), None, None, Some("asdfqwer1236sdfgasdf/zxcvasdfqwer123"))) must throwA[Exception]("No repository found under this GitHub path")
+        await(binTray.createPackage("webjars", "test", repo, "foo description", Seq("test"), Set("MIT"), new URI("http://github.com/webjars/webjars.git"), None, None, Some("asdfqwer1236sdfgasdf/zxcvasdfqwer123"))) must throwA[Exception]("No repository found under this GitHub path")
       }
       "create a package" in {
-        val result = await(binTray.createPackage("webjars", "test", "foo", "foo description", Seq("test"), Set("MIT"), new URI("http://github.com/webjars/webjars.git"), Some(new URL("http://webjars.org")), Some(new URL("http://github.com/webjars/webjars/issues")), Some("webjars/webjars")))
+        val result = await(binTray.createPackage("webjars", "test", repo, "foo description", Seq("test"), Set("MIT"), new URI("http://github.com/webjars/webjars.git"), Some(new URL("http://webjars.org")), Some(new URL("http://github.com/webjars/webjars/issues")), Some("webjars/webjars")))
         (result \ "created").asOpt[Date] must beSome
       }
       "get or create package should work" in {
-        val result = await(binTray.getOrCreatePackage("webjars", "test", "foo", "foo description", Seq("test"), Set("MIT"), new URI("http://github.com/webjars/webjars"), Some(new URL("http://webjars.org")), Some(new URL("http://github.com/webjars/webjars/issues")), Some("webjars/webjars")))
+        val result = await(binTray.getOrCreatePackage("webjars", "test", repo, "foo description", Seq("test"), Set("MIT"), new URI("http://github.com/webjars/webjars"), Some(new URL("http://webjars.org")), Some(new URL("http://github.com/webjars/webjars/issues")), Some("webjars/webjars")))
         (result \ "created").asOpt[Date] must beSome
       }
       "create a version" in {
-        val result = await(binTray.createVersion("webjars", "test", "foo", "0.0.1", "Release 0.0.1"))
+        val result = await(binTray.createVersion("webjars", "test", repo, "0.0.1", "Release 0.0.1"))
         (result \ "created").asOpt[Date] must beSome
       }
       "fail to create an existing version" in {
-        await(binTray.createVersion("webjars", "test", "foo", "0.0.1", "Release 0.0.1")) must throwA[BinTray.VersionExists]
+        await(binTray.createVersion("webjars", "test", repo, "0.0.1", "Release 0.0.1")) must throwA[BinTray.VersionExists]
       }
       "overwrite an existing version" in {
-        val result = await(binTray.createOrOverwriteVersion("webjars", "test", "foo", "0.0.1", "Release 0.0.1"))
+        val result = await(binTray.createOrOverwriteVersion("webjars", "test", repo, "0.0.1", "Release 0.0.1"))
         (result \ "created").asOpt[Date] must beSome
       }
       "upload a maven artifact" in {
@@ -54,20 +56,20 @@ class BinTraySpec extends PlaySpecification with GlobalApplication {
           inputStream.close()
           fileBytes
         }.get
-        val result = await(binTray.uploadMavenArtifact("webjars", "test", "foo", "org/webjars/bower/foo/0.0.1/foo-0.0.1.jar", bytes))
+        val result = await(binTray.uploadMavenArtifact("webjars", "test", repo, "org/webjars/bower/foo/0.0.1/foo-0.0.1.jar", bytes))
         (result \ "message").asOpt[String] must beSome("success")
       }
       "sign an artifact" in {
-        val result = await(binTray.signVersion("webjars", "test", "foo", "0.0.1"))
+        val result = await(binTray.signVersion("webjars", "test", repo, "0.0.1"))
         (result \ "message").asOpt[String] must beSome("success")
       }
       "publish an artifact" in {
-        val result = await(binTray.publishVersion("webjars", "test", "foo", "0.0.1"))
+        val result = await(binTray.publishVersion("webjars", "test", repo, "0.0.1"))
         (result \ "files").asOpt[Int] must beSome(2)
       }
 
       step {
-        await(binTray.deletePackage("webjars", "test", "foo"))
+        await(binTray.deletePackage("webjars", "test", repo))
       }
     }
   }
