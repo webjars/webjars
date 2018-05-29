25b239908dd207fd6cbe4b6310e98133f978f543
James Ward <james@jamesward.com>
2018-05-29 09:11:10 -0600

    new https config

--- app/Filters.scala
+++
@@ -1,6 +0,0 @@
-import javax.inject.Inject
-
-import play.api.http.DefaultHttpFilters
-import play.filters.gzip.GzipFilter
-
-class Filters @Inject() (gzip: GzipFilter) extends DefaultHttpFilters(gzip)
